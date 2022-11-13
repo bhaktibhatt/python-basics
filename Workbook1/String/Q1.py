4e6ae364de30922c3ee9edf3b690f5dcd5d25f93
Bhakti <bhaktinirbhaybhatt@gmail.com>
2022-11-13 22:17:47 +0530

    Workbook1

--- Workbook1/String/Q1.py
+++ Workbook1/String/Q1.py
@@ -6,3 +6,5 @@ program?
 "He said, "GO" John"
 'Well who's is this?'
 '''
+'''by Adding \ before a quote'''
+print('It\'s Python\'\n"\"Python\" is easy\"\n"He said, \"GO\" John\"\n\'Well who\'s is this?')
